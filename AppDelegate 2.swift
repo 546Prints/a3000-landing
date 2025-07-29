9f0ab16461291467958432720b75acc5c844977c
546Prints <546prints@gmail.com>
2025-07-29 19:43:45 -0400

    Delete AppDelegate 2.swift

--- AppDelegate 2.swift
+++
@@ -1,6 +0,0 @@
-// AppDelegate placeholder
-import UIKit
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool { return true }
-}
