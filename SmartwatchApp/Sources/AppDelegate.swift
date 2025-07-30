f031a720960994f7fb69d9e454b86df7d9456567
546Prints <546prints@gmail.com>
2025-07-29 20:58:49 -0400

    Update AppDelegate.swift

--- SmartwatchApp/Sources/AppDelegate.swift
+++ SmartwatchApp/Sources/AppDelegate.swift
@@ -1,1 +1,25 @@
+import UIKit
 
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = UIHostingController(rootView: ContentView())
+    window.makeKeyAndVisible()
+    self.window = window
+    return true
+  }
+}
+
+import SwiftUI
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, SmartwatchApp!")
+      .padding()
+  }
+}
